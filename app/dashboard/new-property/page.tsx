3247d4fa8fade498e3a8176c11166bea9a6e2682
regisfilhodev <filhoregis96@gmail.com>
2026-06-21 21:05:26 -0300

    fix: corrigindo o use client

--- app/dashboard/new-property/page.tsx
+++ app/dashboard/new-property/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 export default async function NewPropertyPage({ 
   searchParams 
 }: { 
@@ -12,8 +14,6 @@ export default async function NewPropertyPage({
     </Suspense>
   )
 }
-
-"use client"
 
 import type React from "react"
 import { Suspense } from "react"
